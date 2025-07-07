a485e3030ff63a1cdce8ad2dbf296716871d7931
Ikechukwu Nwosu <ikechukwu.n@outlook.com>
2025-07-07 02:32:27 -0400

    Add create_igbo_word view and template for adding new Igbo words

--- vocab/urls.py
+++ vocab/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'), #maps to home page /
     path('alphabet/', views.alphabet_list, name='alphabet_list'), #/alphabet/
     path('words/', views.word_list, name='word_list'), #/words/
+    path('add-word/', views.create_igbo_word, name='add_word'),
 ]
--- vocab/views.py
+++ vocab/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import IgboAlphabet, IgboWord
 from django.http import HttpResponse
-
+from .forms import IgboWordForm
 # Create your views here.
 
 def home(request):
@@ -17,3 +17,13 @@ def alphabet_list(request):
 def word_list(request):
     words = IgboWord.objects.prefetch_related('dialectword_set').all()
     return render(request, 'vocab/word_list.html', {'words': words})
+
+def create_igbo_word(request):
+    if request.method == 'POST':
+        form = IgboWordForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('word_list')
+        else:
+            form = IgboWordForm()
+        return render(request, 'vocab/create_word.html', {'form': form})
