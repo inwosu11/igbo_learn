d266c3980050981180946714fd4f9aa200787a5c
Ikechukwu Nwosu <ikechukwu.n@outlook.com>
2025-07-09 00:45:20 -0400

    Add URL pattern for word detail view with slug parameter

--- vocab/urls.py
+++ vocab/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('alphabet/', views.alphabet_list, name='alphabet_list'), #/alphabet/
     path('words/', views.word_list, name='word_list'), #/words/
     path('add-word/', views.create_igbo_word, name='add_word'),
+    path('words/<slug:slug>/', views.word_detail, name='word_detail'),
 ]
