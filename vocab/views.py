1461ecf9ce0133f28b0556ccdf81c62542f6287f
Ikechukwu Nwosu <ikechukwu.n@outlook.com>
2025-07-09 00:45:11 -0400

    Add word detail view to display individual Igbo words with dialect variants

--- vocab/views.py
+++ vocab/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from .models import IgboAlphabet, IgboWord
 from django.http import HttpResponse
 from .forms import IgboWordForm
+
 # Create your views here.
 
 def home(request):
@@ -28,3 +29,7 @@ def create_igbo_word(request):
         form = IgboWordForm()
     
     return render(request, 'vocab/create_word.html', {'form': form})
+
+def word_detail(request, slug):
+    word = get_object_or_404(IgboWord.objects.prefetch_related('dialect_words'), slug=slug)
+    return render(request, 'vocab/word_detail.html', {'word':word})
